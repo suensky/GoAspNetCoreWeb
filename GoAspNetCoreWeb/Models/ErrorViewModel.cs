a1fe1c978cb6019f828a0004bb0e1e47d194248a
Huanhuan Sun <husun@microsoft.com>
2018-02-26 14:57:17 -0800

    Add project files.

---
+++ GoAspNetCoreWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GoAspNetCoreWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
